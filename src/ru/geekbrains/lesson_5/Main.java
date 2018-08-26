eff08813a66be93421f22de2d847e165d1b34a5d
Yurok86RUS <yurok.kolesnikov@gmail.com>
2018-08-26 22:51:25 +0500

    finish task

--- src/ru/geekbrains/lesson_5/Main.java
+++ src/ru/geekbrains/lesson_5/Main.java
@@ -5,7 +5,25 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
 
+    Cat[] cats = {new Cat("Vasya", 5), new Cat("Myrzik", 8), new Cat("Kyzya", 15)};
+    Bowl bowl = new Bowl(2);
 
+    for (Cat cat : cats){
+        System.out.println(cat);
+    }
+
+        System.out.println("V miske: " + bowl + "g korma");
+        System.out.println("dobavim v misky 20g korma");
+        bowl.addFood(20);
+        System.out.println("V miske: " + bowl + "g korma");
+        System.out.println("Koty kyshayut:");
+
+        for (Cat cat : cats){
+            cat.eat(bowl);
+            System.out.println(cat);
+            System.out.println(bowl);
+            System.out.println("\n");
+        }
 
     }
 }
